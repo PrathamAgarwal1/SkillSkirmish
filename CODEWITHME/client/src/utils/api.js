b216267c9c26e643c162a97f591886fd92921837
Pratham Agarwal <prathamagarwalvikas@gmail.com>
2025-11-19 19:23:59 +0530

    Delete CODEWITHME directory

--- CODEWITHME/client/src/utils/api.js
+++
@@ -1,9 +0,0 @@
-// client/src/utils/api.js
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: '/api',
-  withCredentials: true, // send cookies automatically
-});
-
-export default api;
